c3bec6e744cf5642d34b5b153bfd3469586a825c
Aditya Rnajan <100189617+r-an-j-an@users.noreply.github.com>
2022-04-14 17:15:25 +0530

    Add files via upload

---
+++ Sentiment.py
@@ -0,0 +1,68 @@
+import tweepy
+from textblob import TextBlob
+import preprocessor as p
+import statistics
+from typing import List
+
+
+auth = tweepy.OAuthHandler('oTqXn5nxuT5CDhxk0Lts3qIjH',
+                           'r8yCyZKDqOZxWNZ2CNwXFimck6OFFJRhxrkJEXBBdIAJ3ZVVCo')
+auth.set_access_token('1449702141069709314-Ukskn3ZYQhUOdKYkd12UNEWLbwAS5v',
+                      'aGxJhaIwzYrsYoW6KqgP1TeKfLQQofbAZwEfYmIg0Vc8T')
+
+api = tweepy.API(auth)
+
+
+def get_tweets(keyword: str) -> List[str]:
+    all_tweets = []
+    for tweet in tweepy.Cursor(api.search_tweets, q=keyword, tweet_mode='extended', lang='en').items(10):
+        all_tweets.append(tweet.full_text)
+    return all_tweets
+
+
+def clean_tweets(all_tweets: List[str]) -> List[str]:
+    tweets_clean = []
+    for tweet in all_tweets:
+        tweets_clean.append(p.clean(tweet))
+    return tweets_clean
+
+
+def get_sentiment(all_tweets: List[str]) -> List[float]:
+    sentiment_scores = []
+    for tweet in all_tweets:
+        blob = TextBlob(tweet)
+        sentiment_scores.append(blob.sentiment.polarity)
+
+    return sentiment_scores
+
+
+def generate_average_sentiment_score(keyword: str) -> int:
+    tweets = get_tweets(keyword)
+    tweets_clean = clean_tweets(tweets)
+    for twt in tweets_clean:
+        print("Tweet", twt)
+        print("\n")
+    sentiment_scores = get_sentiment(tweets_clean)
+    average_score = statistics.mean(sentiment_scores)
+    return average_score
+
+
+if __name__ == "__main__":
+    print("What does the World prefer?")
+    first_keyword = input()
+    print("or")
+    second_keyword = input()
+    print("\n")
+
+    first_score = generate_average_sentiment_score(first_keyword)
+    second_score = generate_average_sentiment_score(second_keyword)
+    print("\n\n\n\n")
+    if(first_score > second_score):
+        print("The humanity prefers", first_keyword)
+    if(second_score > first_score):
+        print("The humanity prefers", second_keyword)
+
+    print("------------------------------------------------------------------------------------------------------")
+    print("\n")
+    print(first_keyword, "has a sentimental score of ", first_score)
+    print(second_keyword, "has a sentimental score of ", second_score)
